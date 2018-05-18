c60eb31f8b9647bd39219c1105e429af1880b943
Adrian Werimo <w.adrian@carreltech.com>
2018-05-18 17:13:05 +0300

    First commit :)

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+$f3 = Base::instance();
+
+//Sent to config.ini
+//$f3->set('DEBUG', 3);
+
+$f3->config('config.ini');
+$f3->config('routes.ini');
+
+class AppController {
+    function beforeroute(){
+        echo 'Before routing.- ';
+    }
+    function afterroute(){
+        echo ' -.After routing';
+    }
+}
+
+class Main extends AppController {
+    function render($f3){
+        $f3->set('name','Hello World');
+        $template=new Template;
+        echo $template->render('template.html');    
+    }
+    function sayhello(){
+        echo 'Hello Babe!';
+    }
+}
+
+class AboutPage extends AppController {
+    function render(){
+        echo 'This is the About Page';
+    }
+}
+/*$f3->set('message','Hello.World!!!');
+
+$f3->route('GET /',
+          function($f3) {
+              echo $f3->get('message');
+          });
+
+$f3->route('GET /Adrian',
+          function() {
+              echo ('Hello Adrian!');
+          });*/
+
+/*$f3->route('GET /', 'Main->render');
+$f3->route('GET /hello', 'Main->sayhello');
+$f3->route('GET /about', 'AboutPage->render');*/
+
+$f3->run();
